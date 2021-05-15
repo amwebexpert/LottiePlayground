491377dddeec5f3de72d07eba542d86db4e12676
Andre Masson <amwebexpert@gmail.com>
2021-05-15 09:21:17 -0400

    Simplified layout

--- app/src/main/java/com/example/lottieplayground/MainActivity.java
+++ app/src/main/java/com/example/lottieplayground/MainActivity.java
@@ -2,27 +2,12 @@ package com.example.lottieplayground;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends Activity {
-
-    LottieAnimationView lottie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        lottie = findViewById(R.id.lottie);
-        lottie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                lottie.playAnimation();
-                Toast.makeText(MainActivity.this, "onClick", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
